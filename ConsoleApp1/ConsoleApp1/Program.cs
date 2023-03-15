eba2ef9657d920aaa16d9d82d6a55917dd3bbf6a
Dima <sdvsvd11@gmail.com>
2023-03-15 23:12:05 +0200

    finish

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFilePath = "D:/1/1in.csv";
+        string outputFilePathTemplate = "D:/1/1out.csv";
+        string dateFormat = "dd/MM/yyyy";
+
+        Func<string, DateTime> getDate = (line) =>
+        {
+            string[] parts = line.Split(',');
+            return DateTime.ParseExact(parts[0], dateFormat, null);
+        };
+
+        Func<string, double> getAmount = (line) =>
+        {
+            string[] parts = line.Split(',');
+            return double.Parse(parts[1]);
+        };
+
+        Action<DateTime, double> writeDailyTotal = (date, total) =>
+        {
+            string outputFilePath = string.Format(outputFilePathTemplate, date.ToString("yyyyMMdd"));
+            using (var writer = new StreamWriter(outputFilePath, true))
+            {
+                writer.WriteLine("{0},{1}", date.ToString(dateFormat), total.ToString("0.00"));
+            }
+        };
+
+        int batchSize = 10;
+        int batchCount = 0;
+        DateTime batchDate = DateTime.MinValue;
+        double batchTotal = 0;
+
+        using (var reader = new StreamReader(inputFilePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                DateTime date = getDate(line);
+                double amount = getAmount(line);
+
+                if (date != batchDate)
+                {
+                    if (batchCount > 0)
+                    {
+                        writeDailyTotal(batchDate, batchTotal);
+                        batchCount = 0;
+                        batchTotal = 0;
+                    }
+                    batchDate = date;
+                }
+
+                batchTotal += amount;
+                batchCount++;
+
+                if (batchCount == batchSize)
+                {
+                    writeDailyTotal(batchDate, batchTotal);
+                    batchCount = 0;
+                    batchTotal = 0;
+                }
+            }
+
+            if (batchCount > 0)
+            {
+                writeDailyTotal(batchDate, batchTotal);
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Drawing.Common;
+
+namespace ImageProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] imagePaths = { "image1.jpg", "image2.jpg", "image3.jpg" }; // список шляхів до зображень
+
+            Func<Bitmap, Bitmap> grayscale = image =>
+            {
+                Bitmap grayscaleImage = new Bitmap(image.Width, image.Height);
+
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+                        int grayscaleValue = (int)(0.2989 * pixelColor.R + 0.5870 * pixelColor.G + 0.1140 * pixelColor.B);
+                        Color grayscaleColor = Color.FromArgb(pixelColor.A, grayscaleValue, grayscaleValue, grayscaleValue);
+                        grayscaleImage.SetPixel(x, y, grayscaleColor);
+                    }
+                }
+
+                return grayscaleImage;
+            };
+
+            Func<Bitmap, Bitmap> invertColors = image =>
+            {
+                Bitmap invertedImage = new Bitmap(image.Width, image.Height);
+
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+                        Color invertedColor = Color.FromArgb(pixelColor.A, 255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B);
+                        invertedImage.SetPixel(x, y, invertedColor);
+                    }
+                }
+
+                return invertedImage;
+            };
+
+            Action<Bitmap> displayImage = image =>
+            {
+                image.Save(Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".jpg"); // зберігаємо оброблене зображення в новому файлі з випадковим іменем
+                Console.WriteLine("Image processed and saved to disk");
+            };
+
+            var processedImages = imagePaths.Select(path => new Bitmap(path))
+                                            .Select(grayscale)
+                                            .Select(invertColors);
+
+            foreach (var image in processedImages)
+            {
+                displayImage(image);
+            }
+        }
+    }
+}
